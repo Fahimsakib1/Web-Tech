9d19ebd8c0364249106af743aa42a280a82bba0a
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2020-12-10 00:55:35 +0600

    Add files via upload

---
+++ FINAL TERM/Final Project/False Railway/View/Footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<table align="center">
+			<tr>
+				<td style="vertical-align: middle; font-weight:bold; color:black; height:14px;";>
+					<h2>Copyright &copy; 2016-2020</h2>
+			
+					<p> <h3 style="color:green; font-weight: bold;font-size: 25px">For More Details Contact : 001122334455 or Visit : <a style = "font-size:27px; font-weight:bold " href="https://www.esheba.cnsbd.com/#/">Website </a> </h3></p>
+				</tr>
+
+			<tr>	
+		<table>
+	</body>
+</html>
